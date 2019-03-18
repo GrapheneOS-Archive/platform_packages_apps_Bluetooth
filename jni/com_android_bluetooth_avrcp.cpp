e146fd8ed03250dfe388667828cfae1455e31cf9
Daniel Micay <danielmicay@gmail.com>
2019-03-17 21:19:00 -0400

    constify JNINativeMethod tables

--- jni/com_android_bluetooth_avrcp.cpp
+++ jni/com_android_bluetooth_avrcp.cpp
@@ -1400,7 +1400,7 @@ static jboolean addToNowPlayingRspNative(JNIEnv* env, jobject object,
   return (status == BT_STATUS_SUCCESS) ? JNI_TRUE : JNI_FALSE;
 }
 
-static JNINativeMethod sMethods[] = {
+static const JNINativeMethod sMethods[] = {
     {"classInitNative", "()V", (void*)classInitNative},
     {"initNative", "()V", (void*)initNative},
     {"cleanupNative", "()V", (void*)cleanupNative},
