eecdcd777151732b6265dac81b900ebfe86bed96
Daniel Micay <danielmicay@gmail.com>
2021-10-10 18:35:08 -0400

    constify JNINativeMethod tables

--- jni/com_android_bluetooth_gatt.cpp
+++ jni/com_android_bluetooth_gatt.cpp
@@ -2290,7 +2290,7 @@ static void gattTestNative(JNIEnv* env, jobject object, jint command,
  */
 
 // JNI functions defined in AdvertiseManager class.
-static JNINativeMethod sAdvertiseMethods[] = {
+static const JNINativeMethod sAdvertiseMethods[] = {
     {"classInitNative", "()V", (void*)advertiseClassInitNative},
     {"initializeNative", "()V", (void*)advertiseInitializeNative},
     {"cleanupNative", "()V", (void*)advertiseCleanupNative},
@@ -2317,7 +2317,7 @@ static JNINativeMethod sAdvertiseMethods[] = {
 };
 
 // JNI functions defined in PeriodicScanManager class.
-static JNINativeMethod sPeriodicScanMethods[] = {
+static const JNINativeMethod sPeriodicScanMethods[] = {
     {"classInitNative", "()V", (void*)periodicScanClassInitNative},
     {"initializeNative", "()V", (void*)periodicScanInitializeNative},
     {"cleanupNative", "()V", (void*)periodicScanCleanupNative},
@@ -2326,7 +2326,7 @@ static JNINativeMethod sPeriodicScanMethods[] = {
 };
 
 // JNI functions defined in ScanManager class.
-static JNINativeMethod sScanMethods[] = {
+static const JNINativeMethod sScanMethods[] = {
     {"registerScannerNative", "(JJ)V", (void*)registerScannerNative},
     {"unregisterScannerNative", "(I)V", (void*)unregisterScannerNative},
     {"gattClientScanNative", "(Z)V", (void*)gattClientScanNative},
@@ -2359,7 +2359,7 @@ static JNINativeMethod sScanMethods[] = {
 };
 
 // JNI functions defined in GattService class.
-static JNINativeMethod sMethods[] = {
+static const JNINativeMethod sMethods[] = {
     {"classInitNative", "()V", (void*)classInitNative},
     {"initializeNative", "()V", (void*)initializeNative},
     {"cleanupNative", "()V", (void*)cleanupNative},
